cb6d12b6df185a41238e2bbba446404db219afa8
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-01-26 23:18:56 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import Navbar from "../components/Navbar" 
+import Footer from "../components/Footer" 
+import index from "./index" 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return( 
+    <>
+  <Navbar/>
+  <Component {...pageProps} />
+
+  <Footer/>
+  
+  <Script src="https://kit.fontawesome.com/628fde244b.js"></Script>
+    </>
+
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,117 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import Navbar from "../components/Navbar" 
+import Footer from "../components/Footer" 
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
+import iphone50 from "../public/img/iphone15pro.webp"
+import realme9pro from "../public/img/realme9pro.jpg"
+import oneplusnordc3lite from "../public/img/oneplusnordc3lite.jpg"
+import realmenarzo60 from "../public/img/realmenarzo60.jpg"
+import redminote35g from "../public/img/redminote135g.jpg"
+import realmenarzo60x from "../public/img/realmenarzo60x.webp"
+import googlepixel7pro from "../public/img/googlepixel7pro.jpg"
+import samsunggalaxys24plus from "../public/img/samsunggalaxys24+.jpg"
+
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div >
+
+      <Head>
+        <title>Amikart</title>
+      </Head>
+       {/* <Navbar/> */}
+       
+       <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={iphone50} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Apple</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">iphone 8</h2>
+          <p className="mt-1">$16.00</p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={realme9pro} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Readmi</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Readmi 9 Pro</h2>
+          <p className="mt-1">$21.15</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={oneplusnordc3lite} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">OnePlus</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">OnePlus Nord CE 3 Lite 5G</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
       </div>
-    </main>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={realmenarzo60} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Realme</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Realme narzo 60 5G</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={samsunggalaxys24plus} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Samsung</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Samsung Galaxy24+</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+          <Image src={googlepixel7pro} alt="" style={{height:"17.5rem",padding:"1rem 2rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Google</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Google Pixel PRO 5G</h2>
+          <p className="mt-1">$21.15</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+        <Image src={realmenarzo60x} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Realme</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Realme Narzo 60X</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-50 rounded overflow-hidden">
+        <Image src={redminote35g} alt="" style={{height:"16 rem",width:"17rem",border:"2px solid lightgrey",borderRadius:"1rem"}}></Image>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Redmi</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Redmi note 13 5G</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+       {/* <Footer/> */}
+    </div>
   );
 }
