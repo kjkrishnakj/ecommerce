324fa0d6faa923bff6e79a644653b3725994abc6
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-01-30 01:13:27 +0530

    cart work

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,8 @@ import React, { useRef } from "react"
 import Image from "next/image"
 // import logo from "../public/favicon (1).ico"
 import logo from "../public/ak_logo3.png"
-const Navbar = () => {
-
+const Navbar = ({cart,addToCart,removeFromCart,ClearCart,SubTotal}) => {
+// console.log(cart,addToCart,removeFromCart,ClearCart,SubTotal);
 
     const toggleCart = () => {
         if (ref.current.classList.contains("translate-x-full")) {
@@ -50,95 +50,25 @@ const Navbar = () => {
                 <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-xl"><i className="fa-solid fa-arrow-right-long"></i></span>
 
                 <ol className="list-decimal">
-                    <li>
+                    {Object.keys(cart).length ==0 && <div className="my-4 text-xl">Cart is Empty!</div>
+                    }
+                   {Object.keys(cart).map((k)=>{ return <li key={k}>
                         <div className="flex items-center justify-between my-5">
                             <div className="w-2/3">
-                                Realme narzo 60
+                                {cart[k].name}
                             </div>
                             <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
+                            <i onClick={()=>{
+                                removeFromCart(k,1,cart[k].price,cart[k].name,cart[k].variant)
+                            }} className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  {cart[k].qty} <i onClick={()=>{
+                                addToCart(k,1,cart[k].price,cart[k].name,cart[k].variant)
+                            }} className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
                             </div>
                         </div>
 
-                    </li>
+                    </li>})}
                    
                    
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
-                   
-                   
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
-                   
-                   
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
-                   
-                   
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
-                   
-                   
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
-                   
-                   
-                    <li>
-                        <div className="flex items-center justify-between my-5">
-                            <div className="w-2/3">
-                                Realme narzo 60
-                            </div>
-                            <div className="w-1/3 flex items-center justify-center">
-                            <i className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  1 <i className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
-                            </div>
-                        </div>
-
-                    </li>
                    
                    
                     
@@ -147,9 +77,10 @@ const Navbar = () => {
 
                 <button className="text-white flex mx-2 bg-indigo-500 border-0 py-2 px-3 focus:outline-none hover:bg-indigo-600 mt-4 rounded text-lg"> <span><i class="fa-solid fa-bag-shopping mr-3"></i></span>Checkout</button>
       
-                <button className="text-white flex mx-2 bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 mt-4 rounded text-lg">Clear</button>
+                <button onClick={ClearCart} className="text-white flex mx-2 bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 mt-4 rounded text-lg">Clear</button>
       
                 </div>
+                <div><h4>SubTotal: </h4>{SubTotal}</div>
 
             </div>
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,75 @@ import Script from "next/script";
 import Navbar from "../components/Navbar" 
 import Footer from "../components/Footer" 
 import index from "./index" 
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  const [cart,setCart] = useState({})
+  const [subTotal,setSubTotal] = useState(0)
+
+  useEffect(()=>{
+    try{
+
+      if(localStorage.getItem("cart")){
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      }
+    }
+    catch(e){
+      console.error(e);
+      localStorage.clear()
+
+    }
+  },[])
+
+  const saveCart=(myCart)=>{
+    localStorage.setItem("cart",JSON.stringify(myCart))
+    let subt =0;
+    let keys= Object.keys(myCart)
+    for(let i =0;i<keys.length;i++){
+
+      subt+=myCart[keys[i]].price* myCart[keys[i]].qty;
+    }
+    setSubTotal(subt);
+  }
+  
+  const addToCart  = (itemCode,qty,price,name,variant)=>{
+    let newCart = cart;
+    if(itemCode in cart){ 
+      newCart[itemCode].qty = cart[itemCode].qty+qty
+    }
+    else{
+      newCart[itemCode] = {qty:1,price,name,variant }
+
+    }
+    setCart(newCart);
+    saveCart(newCart)
+  }
+  
+  const removeFromCart  = (itemCode,qty,price,name,variant)=>{
+    let newCart = JSON.parse(JSON.stringify(cart));
+    if(itemCode in cart){
+      newCart[itemCode].qty = cart[itemCode].qty-qty
+    }
+    if(newCart[itemCode]["qty"]<=0){
+      delete newCart[itemCode]
+    } 
+    setCart(newCart);
+    saveCart(newCart)
+  }
+
+
+ const clearCart = ()=>{
+  setCart({})
+  saveCart({})
+ }
+
+
   return( 
     <>
-  <Navbar/>
-  <Component {...pageProps} />
-  <Footer/>
+  <Navbar cart = {cart} addToCart ={addToCart} removeFromCart = {removeFromCart} ClearCart = {clearCart} SubTotal = {subTotal}/>
+  <Component cart = {cart} addToCart ={addToCart} removeFromCart = {removeFromCart} ClearCart = {clearCart} SubTotal = {subTotal} {...pageProps} />
+  <Footer cart = {cart} addToCart ={addToCart} removeFromCart = {removeFromCart} ClearCart = {clearCart} SubTotal = {subTotal}/>
   
 
 
