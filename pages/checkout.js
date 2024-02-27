228716afefefda2525ff07ff545d73af62e652f3
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-02-28 01:30:08 +0530

    checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,14 +1,200 @@
 import Image from 'next/image'
 import React from 'react'
-import logo from "../public/ak_logo4.png"
+// import logo from "../public/ak_logo4.png"
 import Link from 'next/link'
 
-const Checkout = () => {
+import mongoose from "mongoose";
+
+import Product from "../models/Product";
+import Head from 'next/head';
+import Script from 'next/script';
+import samsunggalaxys24plus from "../public/img/samsunggalaxys24+.jpg"
+
+
+const Checkout = ({ cart, addToCart, removeFromCart, ClearCart, SubTotal, products }) => {
+
+  const intiatePayment = () => {
+    let txnToken
+    let amount;
+    var config = {
+      "root": "",
+      "flow": "DEFAULT",
+      "data": {
+        "orderId": "Math.random()", /* update order id */
+        "token": txnToken, /* update token value */
+        "tokenType": "TXN_TOKEN",
+        "amount": "amount" /* update amount */
+      },
+      "handler": {
+        "notifyMerchant": function (eventName, data) {
+          console.log("notifyMerchant handler function called");
+          console.log("eventName => ", eventName);
+          console.log("data => ", data);
+        }
+      }
+    };
+
+    window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+      // after successfully updating configuration, invoke JS Checkout
+      window.Paytm.CheckoutJS.invoke();
+    }).catch(function onError(error) {
+      console.log("error => ", error);
+    });
+
+  }
   return (
     <div>
-       checkout
-    </div>
+      <Head><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" /></Head>
+
+      <Script type="application/javascript" src={`${process.env.PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.PAYTM_MID}.js`} onload="onScriptLoad();" crossorigin="anonymous">
+
+      </Script>
+
+
+
+      <div className="mb-6">
+        <div className="flex flex-col mb-10 items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+        </div>
+        <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+          <div className="px-4 pt-8">
+            <p className="text-xl font-medium">Order Summary</p>
+            <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
+            {/* <div className=" "> */}
+            {/* <ol type='1'>
+              {Object.keys(cart).length == 0 && <div className="">Your Cart is Empty!</div>}
+              {Object.keys(cart).map((k) => {
+                return <li key={k}>
+                       {cart[k].title} 
+                  <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+                    <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={cart[k].img} alt="" />
+                    <div className="flex w-full flex-col px-4 py-4">
+                      <div className="font-semibold">{cart[k].brand}</div>
+                      <p className="mt-auto text-lg font-bold">₹{cart[k].price}</p>
+                    </div>
+                  </div>
+                </li>
+              })}
+              </ol> */}
+            <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
+
+              <ol className="list-decimal">
+                {Object.keys(cart).length == 0 && <div className="my-4 text-xl">Cart is Empty!</div>
+                }
+                {Object.keys(cart).map((k) => {
+                  return <li key={k}>
+                    <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+                      {/* {/* <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={cart[k].img} alt="" />
+                    <Image></Image> */}
+                      <img src={cart[k].img} alt="" className="m-2 h-24 w-28 rounded-md border object-cover object-center" style={{}}></img>
+
+                      {/* <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={samsunggalaxys24plus} alt="" /> */}
+                      {/* <Image src={samsunggalaxys24plus} alt="" style={{ height: "26rem", width: "25rem" }}></Image> */}
+                      {/* <div className="w-2/3">
+                        {cart[k].price} {cart[k].name}
+                      </div> */}
+                      <div className="flex w-full flex-col px-4 py-4">
+                        <div className="font-semibold">{cart[k].name}</div>
+                        <div className="text-gray-500">{cart[k].variant}</div>
+                        <p className="mt-auto text-lg font-bold">₹{cart[k].price}</p>
+                      </div>
+
+                      <div className="w-1/3 flex items-center justify-center">
+                        <i onClick={() => {
+                          removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].variant)
+                        }} className="fa-solid fa-circle-minus cursor-pointer mx-3"></i>  {cart[k].qty} <i onClick={() => {
+                          addToCart(k, 1, cart[k].price, cart[k].name, cart[k].variant)
+                        }} className="fa-solid fa-circle-plus mx-3 cursor-pointer"></i>
+                      </div>
+                    </div>
+
+                  </li>
+                })}
+
+              </ol>
+            </div>
+
+
+          </div>
+          <div className="mt-10 bg-gray-100 px-4 pt-8 lg:mt-0">
+            <p className="text-xl font-medium">Delivery Details</p>
+            <p className="text-gray-400">Complete your order by providing your delivery details.</p>
+
+
+
+            <form className="max-w-md mx-auto">
+              <div className="relative z-0 w-full mb-5 group">
+                <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full Name</label>
+              </div>
+              <div className="relative z-0 w-full mb-5 group">
+                <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+              </div>
+
+              <div className="grid md:grid-cols-2 md:gap-6">
+                <div className="relative z-0 w-full mb-5 group">
+                  <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                  <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">State</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                  <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                  <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">City</label>
+                </div>
+              </div>
+              <div className="grid md:grid-cols-2 md:gap-6">
+                <div className="relative z-0 w-full mb-5 group">
+                  <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                  <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                  <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                  <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Landmark</label>
+                </div>
+              </div>
+            </form>
+
+
+            {<div className="">
+              <div className="mt-6 border-t border-b py-2">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                  <p className="font-semibold text-gray-900">₹{SubTotal}</p>
+                </div>
+              </div>
+              <div className="mt-2  border-b py-2">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-gray-900">Shipping Charges</p>
+                  <p className="font-semibold text-gray-900">₹50</p>
+                </div>
+              </div>
+              <div className="mt-6 flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Total</p>
+                <p className="text-2xl font-semibold text-gray-900">₹{SubTotal + 50}</p>
+              </div>
+            </div>}
+            <Link href={'/checkout'}><button onClick={intiatePayment} className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">Place Order</button>
+            </Link></div>
+        </div>
+
+
+
+      </div >
+    </div >
   )
 }
-
 export default Checkout
+
+
+// export async function getServerSideProps(context) {
+//   if (!mongoose.connections[0].readyState) {
+//     await mongoose.connect(process.env.MONGO_URI)
+//   }
+
+
+//   let products= await Product.find({})
+
+
+//   return {
+//     props: { products: products }
+//   }
+// }
--- pages/order.js
+++ pages/order.js
@@ -7,68 +7,68 @@ const Order = ({ cart, addToCart, removeFromCart, ClearCart, SubTotal }) => {
   return (
     <div>
       <section className="text-gray-600 body-font overflow-hidden">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="lg:w-4/5 mx-auto flex flex-wrap">
-      <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">{}</h2>
-        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Order Id - #89777</h1>
-        <p className="leading-relaxed text-green-600 mb-4">Yay! Your order has been succesfully placed</p>
-            <div class="flex mb-4">
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Description</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Quantity</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Total</a>
-        </div>
-      
-        {/* <div className="flex  border-gray-200 py-2">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">{ }</h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Order Id - #89777</h1>
+              <p className="leading-relaxed text-green-600 mb-4">Yay! Your order has been succesfully placed</p>
+              <div className="flex mb-4">
+                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Description</a>
+                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Quantity</a>
+                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Total</a>
+              </div>
+
+              {/* <div className="flex  border-gray-200 py-2">
           <span className="text-gray-500">Total </span>
           <span className="ml-auto text-gray-900">{SubTotal}</span>
         </div> */}
-        <div className="flex justify-center border-b-2 border-gray-200 py-2">
-          <span className="text-gray-500">Galaxy 24+</span>
-          <span className=" ml-auto text-center text-gray-500">1</span>
-          <span className="ml-auto text-gray-900">₹499</span>
-        </div>  
-        <div className="flex border-b-2 border-gray-200 py-2">
-          <span className="text-gray-500">Narzo 60 5G</span>
-          <span className=" ml-auto text-center text-gray-500">1</span>
-          <span className="ml-auto text-gray-900">₹499</span>
-        </div>  
-        <div className="flex border-b-2 border-gray-200 py-2">
-          <span className="text-gray-500">name</span>
-          <span className=" ml-auto text-center text-gray-500">1</span>
-          <span className="ml-auto text-gray-900">₹499</span>
-        </div>  
-          
-        <div className="flex flex-col mt-4">
-          <span className="title-font font-medium  text-2xl text-gray-900">Total : ₹{SubTotal}</span>
-          <div className="my-5">
-            <button className="flex  text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Track order</button>
-         
-            </div>
-        </div>
-      </div>
-      <Image src={samsunggalaxys24plus} alt="" style={{ height: "26rem", width: "25rem" }}></Image>
+              <div className="flex justify-center border-b-2 border-gray-200 py-2">
+                <span className="text-gray-500">Galaxy 24+</span>
+                <span className=" ml-auto text-center text-gray-500">1</span>
+                <span className="ml-auto text-gray-900">₹499</span>
+              </div>
+              <div className="flex border-b-2 border-gray-200 py-2">
+                <span className="text-gray-500">Narzo 60 5G</span>
+                <span className=" ml-auto text-center text-gray-500">1</span>
+                <span className="ml-auto text-gray-900">₹499</span>
+              </div>
+              <div className="flex border-b-2 border-gray-200 py-2">
+                <span className="text-gray-500">name</span>
+                <span className=" ml-auto text-center text-gray-500">1</span>
+                <span className="ml-auto text-gray-900">₹499</span>
+              </div>
 
-    </div>
-  </div>
-</section>
+              <div className="flex flex-col mt-4">
+                <span className="title-font font-medium  text-2xl text-gray-900">Total : ₹{SubTotal}</span>
+                <div className="my-5">
+                  <button className="flex  text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Track order</button>
+
+                </div>
+              </div>
+            </div>
+            <Image src={samsunggalaxys24plus} alt="" style={{ height: "26rem", width: "25rem" }}></Image>
+
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
 
 export default Order
- 
+
 
 export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
-      await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI)
   }
 
 
-  let orders = await Orders.find({    })
-   
+  let orders = await Orders.find({})
 
-  return { 
-      props: { orders:orders }
+
+  return {
+    props: { orders: orders }
   }
 }
