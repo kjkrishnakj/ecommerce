748649b3ab2ea8c33cbe9232a5e7669ce8b56ce3
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-07-12 22:18:43 +0530

    Update myorders.js kj

--- pages/api/myorders.js
+++ pages/api/myorders.js
@@ -1,17 +1,27 @@
 import connectDb from "../../middleware/mongoose";
 import Order from "../../models/Order";
-import jsonwebtoken from "jsonwebtoken" 
-const handler =async (req, res)=> {
-    const token = req.body.token
-    const data = jsonwebtoken.verify(token,process.env.JWT_SECRET)
-    let orders = await Order.find({email:data.email})
-    // console.log(data.email);
-    res.status(200).json({orders})
-    // console.log(orders);
-    
-}
+import jsonwebtoken from "jsonwebtoken";
 
+const handler = async (req, res) => {
+    try {
+        const token = req.body.token;
+        if (!token) {
+            return res.status(401).json({ error: "Token must be provided" });
+        }
 
-export default connectDb(handler)
+        const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+        if (!data) {
+            return res.status(401).json({ error: "Invalid token" });
+        }
 
+        let orders = await Order.find({ email: "krishnajaswl@gmail.com"});
+;
 
+        res.status(200).json({ orders });
+    } catch (error) {
+        console.error("Error verifying token or fetching orders:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export default connectDb(handler);
