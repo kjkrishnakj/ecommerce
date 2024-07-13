81d72520f9dc324065136e9d8e67b8b1c4d4a7c7
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-07-13 20:52:59 +0530

    Update myorders.js email kj

--- pages/api/myorders.js
+++ pages/api/myorders.js
@@ -1,27 +1,26 @@
-import connectDb from "../../middleware/mongoose";
-import Order from "../../models/Order";
-import jsonwebtoken from "jsonwebtoken";
+    import connectDb from "../../middleware/mongoose";
+    import Order from "../../models/Order";
+    import jsonwebtoken from "jsonwebtoken";
 
-const handler = async (req, res) => {
-    try {
-        const token = req.body.token;
-        if (!token) {
-            return res.status(401).json({ error: "Token must be provided" });
+    const handler = async (req, res) => {
+        try {
+            const token = req.body.token;
+            if (!token) {
+                return res.status(401).json({ error: "Token must be provided" });
+            }
+
+            const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+            if (!data) {
+                return res.status(401).json({ error: "Invalid token" });
+            }
+
+            let orders = await Order.find({ email: data.email });
+
+            res.status(200).json({ orders });
+        } catch (error) {
+            console.error("Error verifying token or fetching orders:", error);
+            res.status(500).json({ error: "Internal Server Error" });
         }
+    };
 
-        const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-        if (!data) {
-            return res.status(401).json({ error: "Invalid token" });
-        }
-
-        let orders = await Order.find({ email: data.email});
-;
-
-        res.status(200).json({ orders });
-    } catch (error) {
-        console.error("Error verifying token or fetching orders:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-};
-
-export default connectDb(handler);
+    export default connectDb(handler);
