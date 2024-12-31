39aa3b7657f01f534c900f5064210c9c7f0423aa
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-12-31 20:36:57 +0530

    Update index.js object fill v2

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function Home({ products }) {
         itemClass="carousel-item"
         arrows={false}
       >
-      <div className="h-[30vh] sm:h-[55vh] mt-20 w-full">
+      <div className="h-[30vh]  aspect-w-16 aspect-h-9  sm:h-[55vh]  mt-20 w-full">
   <Link href="/product/OnePlus115g">
     <img
       src="/img/OnePlusposter.webp"
@@ -108,7 +108,7 @@ export default function Home({ products }) {
     />
   </Link>
 </div>
-<div className="h-[30vh] sm:h-[55vh] mt-20 w-full">
+<div className="h-[30vh]  aspect-w-16 aspect-h-9  sm:h-[55vh] mt-20 w-full">
   <Link href="/product/IQOONEO7PRO5G">
     <img
       src="/img/iqoo_poster.jpg"
@@ -117,7 +117,7 @@ export default function Home({ products }) {
     />
   </Link>
 </div>
-<div className="h-[30vh] sm:h-[55vh] mt-20 w-full">
+<div className="h-[30vh]  aspect-w-16 aspect-h-9  sm:h-[55vh] mt-20 w-full">
   <Link href="/product/realme12pro5g">
     <img
       src="/img/realme12pro_poster.jpg"
@@ -126,10 +126,19 @@ export default function Home({ products }) {
     />
   </Link>
 </div>
-<div className="h-[30vh] sm:h-[55vh] mt-20 w-full">
+<div className="h-[30vh]  aspect-w-16 aspect-h-9  sm:h-[55vh] mt-20 w-full">
   <Link href="/product/samsaunggalaxys24+">
     <img
       src="/img/s24_poster.webp"
+      alt="image 4"
+      className="h-full w-full object-fill"
+    />
+  </Link>
+</div>
+<div className="h-[30vh]  aspect-w-16 aspect-h-9  sm:h-[55vh] mt-20 w-full">
+  <Link href="/product/samsaunggalaxys24+">
+    <img
+      src="/img/redminote135g_poster.jpg"
       alt="image 4"
       className="h-full w-full object-fill"
     />
@@ -179,8 +188,8 @@ export default function Home({ products }) {
             {Object.keys(products).map((item) => {
 
               return <Link passHref={true} key={products[item]._id} href={`/product/${products[item].slug}`}>
-                <div className=" lg:w-1/2 md:w-1/2 p-2" style={{ width: "6cm", margin: "0.5cm 2cm" }}>
-                  <img src={products[item].img} data-aos="zoom-in" alt="" className="w-full h-full rounded overflow-hidden transform transition-transform duration-300 ease-in-out hover:scale-105 object-fill" style={{ height: "16rem", width: "16rem" }} />
+                <div className=" lg:w-1/2 md:w-1/2 p-2" style={{ width: "8cm", margin: "0.5cm 2cm" }}>
+                  <img src={products[item].img} data-aos="zoom-in" alt="" className="w-full h-full rounded overflow-hidden transform transition-transform duration-300 ease-in-out hover:scale-105 object-fill" style={{ height: "18rem", width: "16rem" }} />
                   <div className="mt-4" data-aos="fade-right">
                     <h3 className="text-gray-500 text-s  tracking-widest title-font mb-1">{products[item].brand}</h3>
                     <h2 className="text-gray-900 title-font text-lg font-medium">{products[item].title}</h2>
